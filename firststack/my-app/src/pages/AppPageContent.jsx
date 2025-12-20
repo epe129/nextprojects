38be84207de00e99241637ea1bca5b44a1fb7d55
epe129 <eemelik985@gmail.com>
2025-12-20 10:45:38 +0200

    commit

--- firststack/my-app/src/pages/AppPageContent.jsx
+++ firststack/my-app/src/pages/AppPageContent.jsx
@@ -3,7 +3,10 @@ import { data } from "react-router-dom";
 
 function Eapp() {
   const [message, setMessage] = useState("");
-
+  const [nimi, setNimi] = useState("");
+  const [kuvaus, setKuvaus] = useState("");
+  const [maara, setMaara] = useState("");
+  
   useEffect(() => {
     fetch("/api/hello")
     .then((res) => res.json())
@@ -11,41 +14,47 @@ function Eapp() {
   }, []);
 
   console.log(message)
-  
-  useEffect(() => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(nimi, kuvaus, maara)
+    
     fetch("/api/post", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username: "moi flask" }),
-      
+      body: JSON.stringify({ nimi: `${nimi}`, kuvaus: `${kuvaus}`, maara: `${maara}`, }),
     });
-  }, []);
+  };
+
+  
 
   return (
     <>
       <h1>Terve, lisää kirja tietokantaan</h1>
-      <div>
-        <form method="post" style={{ width: "180px"}}>
+
+      <div style={{display: "flex", margin: "0 auto", position: "absolute", left: "50%", right: "50%"}}>
+        <form onSubmit={handleSubmit} style={{ width: "180px"}}>
           <label style={{ float: "left"}}>
             kirjanimi: 
           </label>
-          <input style={{ float: "left"}} name="myInput" placeholder="nimi" />
+          <input style={{ float: "left"}} name="kirjaNimi" placeholder="nimi" value={nimi} onChange={e => setNimi(e.target.value)} />
           
           <label style={{ float: "left"}}>
             kirjakuvaus: 
           </label>
-          <textarea style={{ float: "left"}} rows={10} cols={50} name="myInput" placeholder="kuvaus" />
+          <textarea style={{ float: "left"}} rows={10} cols={50} name="kirjanKuvaus" placeholder="kuvaus"  value={kuvaus} onChange={e => setKuvaus(e.target.value)}  />
           
           <label style={{ float: "left"}}>
             kirjajulkaisupäivä: 
           </label>
-          <input style={{ float: "left"}} type="date" placeholder="päivä" />
+          <input style={{ float: "left", marginBottom: "20px"}} type="date" placeholder="päivä"  value={maara} onChange={e => setMaara(e.target.value)} />
           
-          <button style={{ float: "left"}} type="submit">Submit form</button>
-      </form>
+          <button style={{ float: "left", border: "none", backgroundColor: "#00bedbff", fontSize: "1rem", borderRadius: "5px", cursor: "pointer"}} type="submit">Submit form</button>
+        </form>
       </div>
+
     </>
   )
 }
