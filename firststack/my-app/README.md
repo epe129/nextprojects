5aec6c9e76185ef5e84043e354f1706cd0a5bcea
epe129 <eemelik985@gmail.com>
2025-12-22 13:02:44 +0200

    commit

--- firststack/my-app/README.md
+++ firststack/my-app/README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# React + Vite ja flask
 
 # Kirjahylly
 
--- firststack/my-app/src/pages/SecondPage.jsx
+++ firststack/my-app/src/pages/SecondPage.jsx
@@ -1,22 +1,51 @@
 import React, { useState, useEffect } from "react";
 
 
+
 function First() {
-  // const [message, setMessage] = useState("");
-  
+  const [message, setMessage] = useState("");
+
   useEffect(() => {
-    fetch("/api/hello")
-    .then((res) => res.json())
-    .then((data) => data.forEach((element) => console.log(element)))
-  }, []);
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data));
+  }, [])
 
- 
+  const renderedItems = [];
 
+  for (let i = 0; i < message.length; i++) {
+    renderedItems.push(<div style={{
+      width: "30%"
+    }} key={i}>
+      
+      <br/>
+
+      <span style={{fontSize: "2rem"}}>Nimi: {message[i][0]}</span>
+      
+      <br/>
+      
+      <span style={{fontSize: "2rem"}}>tekija: {message[i][1]}</span>
+      
+      <br/>
+      
+      <span style={{fontSize: "2rem"}}>kuvaus: {message[i][2]}</span>
+      
+      <br/>
+      
+      <span style={{fontSize: "2rem"}}>päivämäärä: {message[i][3]}</span>
+      
+      <br/>
+      
+      <span style={{fontSize: "2rem"}}>hinta: {message[i][4]}</span>
+      
+      </div>);
+  }
+  
   return (
     <>
       <h1>hei, katso kirjoja tietokannasta</h1>
-
-      <div>
+      <div style={{ float: "none", textAlign: "left"}}>
+        {renderedItems}
       </div>
     </>
   )
